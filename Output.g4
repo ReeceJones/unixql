260011e3402d27ac1a9f26ebbb3069d9aef27fb5
ReeceJones <reece_jones@icloud.com>
2021-08-15 11:33:59 -0700

    Get LS parser working

---
+++ Output.g4
@@ -0,0 +1,17 @@
+grammar Output;
+
+ls : (ANYCOL ANYCOL CRLF) entries+=line+ EOF;
+line : file_mode number_links user group size month day time path+=ANYCOL CRLF;
+
+file_mode : ANYCOL;
+number_links : ANYCOL;
+user : ANYCOL;
+group : ANYCOL;
+size : ANYCOL;
+day : ANYCOL;
+month : ANYCOL;
+time : ANYCOL;
+
+ANYCOL : '"' .+? [^\\]["] | [A-Za-z0-9\-:._]+;
+CRLF : '\r'? '\n';
+WS : ' ' -> skip;
--- include/parsers/ls.h
+++ include/parsers/ls.h
@@ -4,20 +4,17 @@
 #include <vector>
 #include <tuple>
 #include <any>
-#include "LsParser.h"
+#include "OutputParser.h"
+#include "OutputLexer.h"
 #include "antlr4-runtime.h"
 
 namespace Parsers::Ls {
     typedef std::tuple<std::string, std::any> Column;
     typedef std::vector<Column> Columns;
 
-    inline void parseLs(std::string input) {
-        std::cout << "ls parser received input: "<< input << std::endl;
-    }
-
     class  LsParserVisitor : public antlr4::tree::AbstractParseTreeVisitor {
     public:
-        antlrcpp::Any visitOutput(LsParser::OutputContext *context) {
+        antlrcpp::Any visitOutput(OutputParser::LsContext *context) {
             std::vector<Columns> lines;
 
             for (auto line : context->entries) {
@@ -27,7 +24,12 @@ namespace Parsers::Ls {
             return lines;
         }
 
-        antlrcpp::Any visitLine(LsParser::LineContext* context) {
+        antlrcpp::Any visitLine(OutputParser::LineContext* context) {
+            std::string path;
+            for (auto p : context->path) {
+                path += p->getText() + " ";
+            }
+            path.pop_back();
             Columns columns = {
                 Column("mode", context->file_mode()->getText()),
                 Column("number_links", context->number_links()->getText()),
@@ -36,9 +38,20 @@ namespace Parsers::Ls {
                 Column("size", context->size()->getText()),
                 Column("month", context->month()->getText()),
                 Column("time", context->time()->getText()),
-                Column("path", context->path()->getText())
+                Column("path", path)
             };
             return columns;
         }
     };
+
+    inline void parseLs(std::string output) {
+        antlr4::ANTLRInputStream input(output);
+        OutputLexer lexer(&input);
+        antlr4::CommonTokenStream tokens(&lexer);
+        OutputParser parser(&tokens);
+
+        OutputParser::LsContext* tree = parser.ls();
+        LsParserVisitor visitor;
+        visitor.visitOutput(tree);
+    }
 }
